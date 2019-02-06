491684277cbd84ba178e722b5d9fa84f68202baa
hazel <samihazel@outlook.fr>
2019-02-06 18:42:05 +0100

    Initial Commit

---
+++ BLETest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLETest
+//
+//  Created by macbook on 06/02/2019.
+//  Copyright © 2019 assil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
